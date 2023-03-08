4cd57e25198078901db517ebfd4c122da2e2437c
D9ntnam <namnguyenth0311@gmail.com>
2023-03-08 17:33:57 +0700

    inital commit

---
+++ App/Controllers/DemoController.cs
@@ -0,0 +1,26 @@
+﻿using App.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Demo> Get()
+        {
+            Demo demo = new Demo();
+            demo.Name = "Test";
+            demo.Description = "Description Test";
+
+
+            return Ok(new { 
+                Name = demo.Name,
+                Description= demo.Description
+            });
+        }
+    }
+}
---
+++ App/Dto/Demo.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Dto
+{
+    public class Demo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
